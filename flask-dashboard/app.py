8ce9d6cdddd1e8874b32cb1c9700c00deac65b9c
lee1903 <lee1903@purdue.edu>
2017-09-30 23:57:20 -0400

    recieve data array from python script

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
-from data import getTimelineData, testFunc
+from data import getTimelineData
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	#data = getTimelineData("ME")
 	data = testFunc()
-	print data
 	#use a default org?
 	return render_template('index.html')
 
 @app.route("/<org>")
 def main(org):
-	data = testFunc()
+	data = getTimelineData(org)
 	#get result and pass with render
 	return render_template('index.html', data=data)
 
