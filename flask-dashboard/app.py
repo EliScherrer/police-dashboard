de5c4aea87af4af78aa478e4b9ec631b50ae750e
EliScherrer <escherre@purdue.edu>
2017-09-30 22:54:27 -0400

    messing with flask routes

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
-from './../data.py' import getTimelineData
+from data import getTimelineData, testFunc
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+	#data = getTimelineData("ME")
+	data = testFunc()
+	print data
 	#use a default org?
 	return render_template('index.html')
 
 @app.route("/<org>")
 def main(org):
-	function(org)
+	data = testFunc()
 	#get result and pass with render
-	return render_template('index.html', org=org)
+	return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run()
